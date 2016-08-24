34a896148249bb5652164e63d6e6f4b3d9138062
LiuXinQian <1613565807@qq.com>
2016-08-24 16:04:39 +0800

    修改

---
+++ Example/Pods/Target Support Files/Pods-XQProgressHUD_Example/Pods-XQProgressHUD_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_XQProgressHUD_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_XQProgressHUD_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-XQProgressHUD_Tests/Pods-XQProgressHUD_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XQProgressHUD_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XQProgressHUD_Tests
+@end
